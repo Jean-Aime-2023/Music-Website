21d490e93a22eb4c5e235e79aab8e90bfe5506ca
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2023-07-30 19:54:35 +0200

    First Commit

---
+++ file2.js
@@ -0,0 +1,13 @@
+class Student{
+    var name;
+    var age;
+    var location;
+     let assign = (name,location,age)=>{
+        this.name=name;
+        this.age = age;
+        this.location - location;
+
+    }
+}
+let names = new Student();
+names.assign()
